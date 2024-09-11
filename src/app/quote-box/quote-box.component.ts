1ba54035829f21b502e6fb82286de2cb96921692
Demi Lune <anshikathakur596@gmail.com>
2024-09-11 10:11:00 +0530

    Update quote-box.component.ts

--- src/app/quote-box/quote-box.component.ts
+++ src/app/quote-box/quote-box.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+import { quotes } from '../quotes';
+
+// const randomIndex = Math.floor(Math.random() * quotes.length); 
 
 @Component({
   selector: 'app-quote-box',
@@ -6,7 +9,29 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './quote-box.component.html',
   styleUrl: './quote-box.component.css'
+  
 })
 export class QuoteBoxComponent {
+  // currentQuote = quotes[randomIndex];
+  // quoteText = this.currentQuote.text;
+  // get author() {
+  //   return '~ ' + this.currentQuote.author;
+  // }
+  
+  randomIndex = signal(Math.floor(Math.random() * quotes.length));
+  currentQuote = signal(quotes[this.randomIndex()]);
+  quoteText = computed(() => this.currentQuote().text);
+
+  author = computed(() => '~ ' + this.currentQuote().author);
+
+  getRandomQuote() {
+    // const randomIndex = Math.floor(Math.random() * quotes.length);  
+    // this.currentQuote = quotes[randomIndex];
+    // this.quoteText = this.currentQuote.text;
+  
+    this.randomIndex.set(Math.floor(Math.random() * quotes.length));
+    this.currentQuote.set(quotes[this.randomIndex()]);
+    this.quoteText = computed(() => this.currentQuote().text);
+  }
 
 }
